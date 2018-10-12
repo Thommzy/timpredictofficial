4a545e2a74477fd1a16e66e9e0400bd844c29b01
Tim <mac@MACs-MacBook-Pro.local>
2018-10-12 15:06:27 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Counter from "./components/counter";
+import Header from "./components/header";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <Header />
         <Counter />
       </React.Fragment>
     );
