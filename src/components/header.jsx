3d641dad601f4958f63ce71ec5c2e25379f1bef5
Tim <mac@MACs-MacBook-Pro.local>
2018-10-12 15:08:09 +0100

    changes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Background from "../images/background_image.png";
 
 class Header extends Component {
   render() {
@@ -11,3 +12,15 @@ class Header extends Component {
 }
 
 export default Header;
+
+var sectionStyle = {
+  width: "100%",
+  height: "400px",
+  backgroundImage: "url(" + { Background } + ")"
+};
+
+class Section extends Component {
+  render() {
+    return <section style={sectionStyle} />;
+  }
+}
