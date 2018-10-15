837903510b83d9ac0a62b20f293d1da8d5946a42
Tim <mac@MACs-MacBook-Pro.local>
2018-10-15 15:02:24 +0100

    chang

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from "react";
 import Counter from "./components/counter";
 import Header from "./components/header";
 import "./App.css";
+import { Container } from "reactstrap";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <Header />
-        <Counter />
+        <Container>
+          <Header />
+          <Counter />
+        </Container>
       </React.Fragment>
     );
   }
